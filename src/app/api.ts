5aca794fe411e6c435401f464a72e332b540cfb1
Scott Cooper <scttcper@gmail.com>
2016-10-26 18:17:32 -0700

    unique and most heard

--- src/app/api.ts
+++ src/app/api.ts
@@ -27,18 +27,15 @@ export class Api {
       .catch(this.handleError);
   }
 
+  mostHeard(channelName: string): Promise<Stream[]> {
+    return this.http.get(`${environment.api}/recent/${channelName}`)
+      .toPromise()
+      .then(res => res.json())
+      .catch(this.handleError);
+  }
+
   private handleError(error: Response | any) {
-    // In a real world app, we might use a remote logging infrastructure
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Promise.reject(errMsg);
+    console.error(error);
   }
 
 }
--- src/app/stream/stream.component.ts
+++ src/app/stream/stream.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { Subscription } from 'rxjs/Subscription';
+import * as _ from 'lodash';
 
 import { Api } from '../api';
 import { Channel } from '../channel';
@@ -16,6 +17,10 @@ export class StreamComponent implements OnInit, OnDestroy {
 
   channels: Channel[];
   recent: Stream[];
+  mostHeard: Stream;
+  mostTimesHeard: number;
+  unique: number;
+  total: number;
 
   private sub: Subscription;
 
@@ -31,8 +36,19 @@ export class StreamComponent implements OnInit, OnDestroy {
       // get segment id from route
       let channelName = params['channelName'];
       this.api.currentChannel.next(channelName);
-      this.api.getRecent(channelName).then((recent => this.recent = recent));
+      this.api.getRecent(channelName).then((recent => this.setup(recent)));
     });
+  }
+  setup(recent) {
+    this.recent = recent;
+    this.total = recent.length;
+    const songIds = _.map(this.recent, 'songId');
+    const heardTimes = _.countBy(songIds);
+    const heardTimesPairs = _.toPairs(heardTimes);
+    const mostHeard = _.maxBy(heardTimesPairs, _.last);
+    this.mostHeard = _.find(this.recent, {songId: mostHeard[0]});
+    this.mostTimesHeard = mostHeard[1];
+    this.unique = _.keys(heardTimes).length;
   }
   ngOnDestroy() {
     this.sub.unsubscribe();
